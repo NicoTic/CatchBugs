fd992988faf8eb0a4bcd5ede76d99863456d9dd5
蒋小泉 <jiangxq@travelsky.com>
2019-06-27 13:21:12 +0800

    first commit

---
+++ CatchBug/app/src/main/java/demo/MainActivity.java
@@ -0,0 +1,17 @@
+package demo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.demo.catchbug.R;
+
+public class MainActivity extends AppCompatActivity {
+    private static String asda = "dassda";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
